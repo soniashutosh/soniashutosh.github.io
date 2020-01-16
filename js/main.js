aecd6cd26dc3def15e54c24d55c30392f4c7c2d0
Ashutosh Soni <46646804+ashutosh0699@users.noreply.github.com>
2020-01-16 23:46:17 +0530

    second website

---
+++ js/main.js
@@ -0,0 +1,14 @@
+.extra-div{
+	background: #F7F5F5;
+	border: medium none;
+	padding: 50px!important;
+	border-radius: 3px;
+	transition: 0.3s;
+}
+
+.extra-div:hover{
+	box-shadow: 0 0 20px 0 rgba(180,190,120,0.7);
+	transform: translate(-10px ,7px);
+	/*transform: translateX(-10px);
+	transform: translateY(-15px);*/
+}
